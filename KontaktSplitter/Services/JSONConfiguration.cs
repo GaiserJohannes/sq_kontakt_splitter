9a04b4e62c843159619b03e52c97eb64bced6650
GrafZeppelin <jojo98.hannes@gmail.com>
2020-05-15 20:49:07 +0200

    setup default configuration

--- KontaktSplitter/Services/JSONConfiguration.cs
+++ KontaktSplitter/Services/JSONConfiguration.cs
@@ -1,17 +1,26 @@
 ﻿using KontaktSplitter.Lang;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace KontaktSplitter.Services
 {
     public class JSONConfiguration : IConfiguration
     {
+        private List<Language> CachedLanguage;
+
         public List<Language> GetLanguages()
         {
-            throw new NotImplementedException();
+            if (CachedLanguage == null)
+            {
+                CachedLanguage = JsonSerializer.Deserialize<List<Language>>(File.ReadAllText(""));
+            }
+
+            return CachedLanguage;
         }
 
         public void UpdateLanguage(Language language)
